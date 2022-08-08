b013cda8371cbfe156cbb50b8249799aa07e552f
SingleJohn <SingleJohn@singlelovely.cn>
2022-08-08 17:21:24 +0800

    get config

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -10,6 +10,8 @@ def admin_user(message: Message, bot: TeleBot):
     You can create a function and use parameter pass_bot.
     """
     bot.send_message(message.chat.id, "Hello, admin!")
+    rec_msg = "命令菜单：\n /start 开始 \n /c fid - 爬取指定fid的板块信息\n /get_config - 获取配置文件\n"
+    bot.send_message(message.chat.id, parse_mode="markdown", text=rec_msg)
 
 
 def crawl_plate(message: Message, bot: TeleBot):
@@ -34,7 +36,7 @@ def crawl_plate(message: Message, bot: TeleBot):
         bot.send_message(message.chat.id, "fid不在配置文件中或者格式错误")
 
 
-def get_config(message: Message, bot: TeleBot):
+def read_config(message: Message, bot: TeleBot):
     """
     You can create a function and use parameter pass_bot.
     """
